b7d47001ade89250802b154710e26ba1294301c0
tijmenbaas <tijmenbaas83@outlook.com>
2023-01-02 14:44:22 +0100

    first setup

---
+++ fabric.py
@@ -0,0 +1,21 @@
+from faker import Faker
+import pandas as pd
+from tqdm.auto import tqdm
+
+
+def customer_data(out_file, number_of_records):
+    print("Generating customer data...")
+    for i in tqdm(range(number_of_records)):
+        fake = Faker()
+        data_list = []
+        columns_list = ['first_name', 'last_name', 'email',
+                        'phone_number', 'address', 'city', 'state', 'zipcode', 'country']
+        data_list.append([fake.first_name(), fake.last_name(), fake.email(), fake.phone_number(
+        ), fake.address(), fake.city(), fake.state(), fake.zipcode(), fake.country()])
+
+    print("Writing customer data to csv file...")
+    df = pd.DataFrame(data_list, columns=columns_list)
+    df.to_csv(out_file, mode='w', index=False)
+
+
+customer_data(out_file='customer_data.csv', number_of_records=1000000)
